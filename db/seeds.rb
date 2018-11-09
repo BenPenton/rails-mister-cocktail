aa0bdd16c76f6057e33a370c5d02cd9e589b164b
Ben Penton <benpenton@mac.com>
2018-11-09 16:43:21 +0000

    Added some better css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Cocktail.destroy_all
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
-Cocktail.create(name: "Gin and Tonic")
-Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Gin and Tonic", remote_photo_url: "https://res.cloudinary.com/dmfjwybks/image/upload/v1541776850/gin-tonic-5a8f334b8e1b6e0036a9631d.jpg")
+Cocktail.create(name: "Mojito", remote_photo_url: "https://res.cloudinary.com/dmfjwybks/image/upload/v1541776841/varshesh-joshi-65559-unsplash.jpg")
+puts "Seed created"
